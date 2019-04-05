f6c922598a434a8809c40d32a36e821a92dfef76
Advueu <Slixlps@gmail.com>
2019-04-05 18:24:21 +0200

    SettingsView Implementierung

--- src/Views/SettingsView.java
+++ src/Views/SettingsView.java
@@ -1,4 +1,43 @@
 package Views;
 
-public class SettingsView {
+import Model.SettingsModel;
+import Window.MyButton;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SettingsView extends JPanel implements ActionListener {
+
+    MyButton back,muteMusik;
+
+    SettingsModel settingsModel;
+
+    public SettingsView ( SettingsModel settingsModel)
+    {
+        this.settingsModel = settingsModel;
+
+        back = new MyButton("Back");
+        muteMusik = new MyButton("muteMusik");
+
+        back.addActionListener(this);
+        muteMusik.addActionListener(this);
+
+        this.add(back);
+        this.add(muteMusik);
+    }
+
+
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getSource()== back)
+        {
+            settingsModel.backAction();
+        }
+
+        if (e.getSource() == muteMusik)
+        {
+            settingsModel.muteMusikAction();
+        }
+    }
 }
