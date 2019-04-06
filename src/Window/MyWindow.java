5b3fd163dfcc5a4430ad1749dc75fe9d8db59c19
Manuel <sgt.lelant@gmail.com>
2019-04-06 18:37:22 +0200

    Profile setup is now required

--- src/Window/MyWindow.java
+++ src/Window/MyWindow.java
@@ -1,9 +1,14 @@
 package Window;
 
 import Model.MainMenuModel;
+import Model.Profil;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.concurrent.Flow;
 
 public class MyWindow{
@@ -27,12 +32,18 @@ public class MyWindow{
         window.setVisible(true);
 
         new MainMenuModel();
+
+        makeNewProfile();
+
     }
+
+
     /*//Test von Layouts --> SCHWIERIGE Nummer ;)
     public static  void layoutTry(JPanel jpanel)
     {
         jpanel.setLayout(new GridLayout(0,1));
     }*/
+
 
     public static void setContent(JPanel panel){
         window.setContentPane(panel);
@@ -40,5 +51,25 @@ public class MyWindow{
         window.setVisible(true);
     }
 
+    public static void makeNewProfile(){
+        File file = new File("data/profil.bin");
+        if(!file.exists()){
+            try{
+
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("data/profil.bin"));
+
+                String name = JOptionPane.showInputDialog("Gib deinen Namen ein");
+
+                Profil profil = new Profil(name, 0);
+                objectOutputStream.writeObject(profil);
+                objectOutputStream.close();
+
+            }
+
+            catch(IOException e){
+                System.out.println("Write Fehler: " + e);
+            }
+        }
+    }
 
 }
