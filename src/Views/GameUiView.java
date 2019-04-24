45242ab788c33bb1cf01c956b8b390207e5fe6a4
Advueu <Slixlps@gmail.com>
2019-04-24 18:20:22 +0200

    Einfügen des 2k Hintergrunds und kleine abänderung der GameUIView

--- src/Views/GameUiView.java
+++ src/Views/GameUiView.java
@@ -1,14 +1,26 @@
 package Views;
 
+import Window.MyButton;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GameUiView extends JPanel implements ActionListener {
+    ImageIcon fireImg;
+    MyButton fire;
+
+    public  GameUiView()
+    {
+
+    }
 
 
-
-
+    private void erzeugenOverlay()
+    {
+        fire = new MyButton("","",fireImg);
+        fire.addActionListener(this);
+    }
 
 
 
@@ -20,6 +32,9 @@ public class GameUiView extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if ((e.getSource() == fire))
+        {
 
+        }
     }
 }
--- src/Window/MyWindow.java
+++ src/Window/MyWindow.java
@@ -33,6 +33,7 @@ public class MyWindow{
     public static void main(String args[]) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         new Var();
         new MyWindow();
+
     }
 
 
